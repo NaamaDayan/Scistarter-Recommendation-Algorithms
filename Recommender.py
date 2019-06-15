8228416753b868c437c75e2aa3cf93c83551a379
Naama Dayan <namadayan@gmail.com>
2019-06-15 11:23:25 +0300

    Add user verification stage

--- Recommender.py
+++ Recommender.py
@@ -4,14 +4,24 @@ from CFItemItem import CFItemItem
 from PopularityBased import PopularityBased
 from SVD import SVD
 
+data = pd.read_csv('user_project_matrix.csv')
+data_items = data.drop('user', 1)
+
+
+def get_user_projects(user_id):
+    known_user_likes = data_items.loc[user_id]
+    known_user_likes = known_user_likes[known_user_likes > 0].index.values
+    return known_user_likes
+
 
 def main():
-    data = pd.read_csv('users_projects_full.csv')
-    data_items = data.drop('user', 1)
     # example
     user_index = 68684
     k = 5
     algorithm = CFUserUser(data_items)
+    # verification stage:
+    if len(get_user_projects(user_index)) < 3:  # fresh user
+        algorithm = PopularityBased(data_items)
     recommended_projects = algorithm.get_recommendations(user_index, k)
     return recommended_projects
 
