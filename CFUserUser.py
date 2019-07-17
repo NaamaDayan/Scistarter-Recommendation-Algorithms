7d6a4a3f5ba33aad9cafdf7d39216c4eeb53221d
Naama Dayan <namadayan@gmail.com>
2019-07-17 16:17:10 +0300

    add support in recommending at least one project

--- CFUserUser.py
+++ CFUserUser.py
@@ -7,6 +7,8 @@ class CFUserUser(Strategy):
 
     def __init__(self, data_items):
         self.data_items = data_items
+        self.projects_score = None
+        self.user_index = None
 
     def find_k_similar_users(self, user_id, metric='cosine', k=1000):
         model_knn = NearestNeighbors(k, 1.0, 'brute', 30, metric)
@@ -22,7 +24,7 @@ class CFUserUser(Strategy):
         return known_user_likes
 
     def get_recommendations(self, user_index, k):
-        return self.get_recommendations_helper(user_index, k, 100)
+        return self.get_recommendations_helper(user_index, k, 200)
 
     def get_recommendations_helper(self, user_index, k, k_knn):
         similar_users = self.find_k_similar_users(user_index)
@@ -35,9 +37,18 @@ class CFUserUser(Strategy):
             for user in similar_users.index:
                 projects_scores[s_project] += similar_users.loc[user] * self.data_items.loc[user][s_project]
         projects_scores = sorted(projects_scores.items(), key=lambda x: x[1], reverse=True)  # sort
+        self.projects_score = projects_scores
+        self.user_index = user_index
         recommended_projects = [i[0] for i in projects_scores]
         known_user_projects = self.get_user_projects(user_index)
         recommended_projects = list(filter(lambda x: x not in known_user_projects, recommended_projects))
         while len(recommended_projects) < k:
             recommended_projects = self.get_recommendations_helper(user_index, k, k_knn + 100)  # increase knn_var until sufficient variety of projects
         return recommended_projects[:k]
+
+    def get_highest_online_project(self):
+        from Recommender import is_online_project, recommend_default_online
+        online_similar_projects = list(filter(lambda x: is_online_project(x[0]), self.projects_score))
+        if len(online_similar_projects) == 0:
+            return recommend_default_online(self.user_index)
+        return online_similar_projects[0][0]
--- PopularityBased.py
+++ PopularityBased.py
@@ -6,6 +6,14 @@ class PopularityBased(Strategy):
     def __init__(self, data_items):
         self.data_items = data_items
         self.projects_popularity_scores = data_items.astype(bool).sum(axis=0)
+        self.user = None
 
     def get_recommendations(self, user_index, k):
-        return self.projects_popularity_scores.nlargest(k).index
+        known_user_projects = self.data_items.loc[user_index]
+        known_user_projects = known_user_projects[known_user_projects > 0].index
+        self.user = user_index
+        return list(self.projects_popularity_scores.drop(known_user_projects).nlargest(k).index)
+
+    def get_highest_online_project(self):
+        from Recommender import recommend_default_online
+        return recommend_default_online(self.user)
